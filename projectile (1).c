ac377d04d9de5c75a0d9fd68d17f3c1babf79826
aniya4tay <47803667+aniya4tay@users.noreply.github.com>
2019-05-03 12:19:07 -0400

    Update projectile (1).c

--- projectile (1).c
+++ projectile (1).c
@@ -3,17 +3,17 @@
 #include <cs50.h>
 
 int main(void){
-    int vi=get_int("What is the initial velocity?\n");
-    int angle=get_int("What is the angle?\n");
+    int vi=get_int("What is the initial velocity?\n");// Ask for initial velocity 
+    int angle=get_int("What is the angle?\n");// Ask for angle
     double ay=-9.8;
     double pi= 3.14159;
     
-        float vxi= vi*cos(angle* pi/180);
-        float vyi= vi*sin(angle* pi/180);
+        float vxi= vi*cos(angle* pi/180);// Inital Vx
+        float vyi= vi*sin(angle* pi/180);// Inital Vy
         
-   float vyf=-1*vyi;
-   float vxf= vxi;
-        float t= vyf/ (vyi+ay);
+   float vyf=-1*vyi;// Final Vy is equal to negative final Vy
+   float vxf= vxi;// Final Vx is equal Final Vy
+        float t= vyf/ (vyi+ay);// 
         printf("Total Distance:%f\n", t);
        printf("Airtime:%f\n", (float) vxi * t);
        printf("Max height:%f\n", (float) vyi * (t/2) + (ay/2)* (t/2)*t/2);
