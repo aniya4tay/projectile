83f715db4491f00f0442d0131212df14aed5f3c0
aniya4tay <47803667+aniya4tay@users.noreply.github.com>
2019-05-02 20:30:41 -0400

    Add files via upload

---
+++ projectile (1).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
+int main(void){
+    int vi=get_int("What is the initial velocity?\n");
+    int angle=get_int("What is the angle?\n");
+    double ay=-9.8;
+    double pi= 3.14159;
+    
+        float vxi= vi*cos(angle* pi/180);
+        float vyi= vi*sin(angle* pi/180);
+        
+   float vyf=-1*vyi;
+   float vxf= vxi;
+        float t= vyf/ (vyi+ay);
+        printf("Total Distance:%f\n", t);
+       printf("Airtime:%f\n", (float) vxi * t);
+       printf("Max height:%f\n", (float) vyi * (t/2) + (ay/2)* (t/2)*t/2);
+    }
+    
